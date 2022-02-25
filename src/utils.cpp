0caa4203be210e302f71d239cdfa01a07683a2ac
Rinnegatamante <rinnegatamante@gmail.com>
2022-02-25 21:33:05 +0100

    Initial Vita port.

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,5 +1,9 @@
 #include "common.h"
 
+#ifdef VITA
+#include <psp2/ctrl.h>
+#endif
+
 int distance(int x1, int y1, int x2, int y2)
 {
 	return sq(x1 - x2) + sq(y1 - y2);
@@ -59,7 +63,33 @@ bool collidePointRect(Fixed px, Fixed py, Fixed x, Fixed y, int w, int h)
 
 int isKeyPressed(t_key _k)
 {
+#ifdef VITA
+	if (_k == SDL_SCANCODE_ESCAPE) {
+		return (uint32_t)G_keys & SCE_CTRL_SELECT ? 1 : 0;
+	} else if (_k == SDL_SCANCODE_RETURN) {
+		return (uint32_t)G_keys & SCE_CTRL_CROSS ? 1 : 0;
+	} else if (_k == G_upKey) {
+		return (uint32_t)G_keys & SCE_CTRL_UP ? 1 : 0;
+	} else if (_k == G_downKey) {
+		return (uint32_t)G_keys & SCE_CTRL_DOWN ? 1 : 0;
+	} else if (_k == G_leftKey) {
+		return (uint32_t)G_keys & SCE_CTRL_LEFT ? 1 : 0;
+	} else if (_k == G_rightKey) {
+		return (uint32_t)G_keys & SCE_CTRL_RIGHT ? 1 : 0;
+	} else if (_k == G_fireKey) {
+		return (uint32_t)G_keys & SCE_CTRL_RTRIGGER ? 1 : 0;
+	} else if (_k == G_fragmentKey) {
+		return (uint32_t)G_keys & SCE_CTRL_LTRIGGER ? 1 : 0;
+	} else if (_k == G_polarityKey) {
+		return (uint32_t)G_keys & SCE_CTRL_SQUARE ? 1 : 0;
+	} else if (_k == G_pauseKey) {
+		return (uint32_t)G_keys & SCE_CTRL_START ? 1 : 0;
+	} else {
+		return 0;
+	}
+#else
 	return G_keys[_k];
+#endif
 }
 
 KeyEvent getk(void)
